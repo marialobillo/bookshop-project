993df324cb430d5fcbd7c11ce4eb0ff732546ba4
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2022-12-30 20:23:44 +0100

    test: return 400 when name is not provided

---
+++ api/src/presentation/controllers/signup.spec.ts
@@ -0,0 +1,17 @@
+import { SignUpController } from './signup'
+
+describe('SignUpController', () => {
+  test('Should return 400 if no name is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})
