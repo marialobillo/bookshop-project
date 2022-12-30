5b28495055429b21a58cf73c38792e87fcd25fdc
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2022-12-30 20:32:52 +0100

    test: returning an error when email is not provided

--- api/src/presentation/controllers/signup.spec.ts
+++ api/src/presentation/controllers/signup.spec.ts
@@ -5,7 +5,7 @@ describe('SignUpController', () => {
     const sut = new SignUpController()
     const httpRequest = {
       body: {
-        name: 'any_name',
+        //name: 'any_name',
         email: 'any_email',
         password: 'any_password',
         passwordConfirmation: 'any_password'
@@ -14,5 +14,20 @@ describe('SignUpController', () => {
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('Missing param: name'))
+  })
+
+  test('Should return 400 if no email is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        //email: 'any_email',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: email'))
   })
 })
