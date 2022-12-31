dade745a9fc7278a2c79b393675281575cf74ec4
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2022-12-31 17:01:47 +0100

    test: returning 400 when passwordConfirmation is not provided

--- api/src/presentation/controllers/signup.spec.ts
+++ api/src/presentation/controllers/signup.spec.ts
@@ -46,4 +46,19 @@ describe('SignUpController', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('password'))
   })
+
+  test('Should return 400 if no passwordConfirmation is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email',
+        password: 'any_password',
+        //passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('passwordConfirmation'))
+  })
 })
