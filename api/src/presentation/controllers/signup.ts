13eff8a7887df3245ddd8dd708d461591b131748
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2023-01-08 22:47:13 +0100

    feat: email validator for signup controller

--- api/src/presentation/controllers/signup.ts
+++ api/src/presentation/controllers/signup.ts
@@ -2,8 +2,17 @@ import { MissingParamError } from '../errors/missing-param-error'
 import { badRequest } from '../helpers/http-helper'
 import { HttpRequest, HttpResponse } from '../protocols/http'
 import { Controller } from '../protocols/controller'
+import { EmailValidator } from '../protocols/email-validator'
+import { InvalidParamError } from '../errors/invalid-param-error'
 
 export class SignUpController implements Controller {
+
+  private readonly emailValidator: EmailValidator
+
+  constructor(emailValidator: any) {
+    this.emailValidator = emailValidator
+  }
+
   handle (httpRequest: HttpRequest): HttpResponse {
     const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
     for (const field of requiredFields) {
@@ -11,9 +20,10 @@ export class SignUpController implements Controller {
         return badRequest(new MissingParamError(field))
       }
     }
-    return {
-      statusCode: 400,
-      body: new Error('Missing param')
+    const isValid = this.emailValidator.isValid(httpRequest.body.email)
+    if (!isValid) {
+      return badRequest(new InvalidParamError('email'))
     }
+    return badRequest(new InvalidParamError('email'))
   }
 }
