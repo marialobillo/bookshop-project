2549e592a40238ef8b62b137e00f51ab7d96eff7
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2023-01-10 19:03:51 +0100

    feat: server error for emailvalidator

--- api/src/presentation/controllers/signup.ts
+++ api/src/presentation/controllers/signup.ts
@@ -4,6 +4,7 @@ import { HttpRequest, HttpResponse } from '../protocols/http'
 import { Controller } from '../protocols/controller'
 import { EmailValidator } from '../protocols/email-validator'
 import { InvalidParamError } from '../errors/invalid-param-error'
+import { ServerError } from '../errors/server-error'
 
 export class SignUpController implements Controller {
 
@@ -14,16 +15,25 @@ export class SignUpController implements Controller {
   }
 
   handle (httpRequest: HttpRequest): HttpResponse {
-    const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
-    for (const field of requiredFields) {
-      if(!httpRequest.body[field]) {
-        return badRequest(new MissingParamError(field))
+
+    try {
+      const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
+      for (const field of requiredFields) {
+        if(!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field))
+        }
+      }
+      const isValid = this.emailValidator.isValid(httpRequest.body.email)
+      if (!isValid) {
+        return badRequest(new InvalidParamError('email'))
+      }
+    } catch (error) {
+      return {
+        statusCode: 500, 
+        body: new ServerError()
       }
     }
-    const isValid = this.emailValidator.isValid(httpRequest.body.email)
-    if (!isValid) {
-      return badRequest(new InvalidParamError('email'))
-    }
+    
     return badRequest(new InvalidParamError('email'))
   }
 }
