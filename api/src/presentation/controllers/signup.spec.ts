05a512e9be4c17e1b1f383930f04113ec0070593
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2023-01-15 22:01:55 +0100

    test: ensure signupcontroller return 400 when passwords does not match

--- api/src/presentation/controllers/signup.spec.ts
+++ api/src/presentation/controllers/signup.spec.ts
@@ -143,4 +143,19 @@ describe('SignUpController', () => {
     expect(httpResponse.statusCode).toBe(500)
     expect(httpResponse.body).toEqual(new ServerError())
   })
+
+  test('Should returns 400 if password-confirmation fails', () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'invalid_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new InvalidParamError('passwordConfirmation'))
+  })
 })
