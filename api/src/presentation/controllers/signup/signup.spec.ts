d209d11a3f0f3ffa376d31879aaa5ee4fd630ad1
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2023-01-16 19:35:16 +0100

    test: ensure signupcontroller return 200 when valid data provided

--- api/src/presentation/controllers/signup/signup.spec.ts
+++ api/src/presentation/controllers/signup/signup.spec.ts
@@ -191,4 +191,42 @@ describe('SignUpController', () => {
        password: 'any_password',
 	 })
   })
+
+  test('Should returns 500 if AddAccount throws', () => {
+    const { sut, addAccountStub } = makeSut()
+    jest.spyOn(addAccountStub, 'add').mockImplementationOnce(() => {
+      throw new Error()
+    })
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+	      email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(500)
+    expect(httpResponse.body).toEqual(new ServerError())
+  })
+
+  test('Should return 200 if valid data is provided', () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'invalid_email',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(200)
+    expect(httpResponse.body).toEqual({
+      id: 'valid_id',
+      name: 'valid_name',
+      email: 'valid_email@mail.com',
+      password: 'valid_password'
+    })
+  })
 })
