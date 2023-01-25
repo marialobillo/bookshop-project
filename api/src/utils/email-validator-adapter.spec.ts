44474ce29d0d2096b043da1bd4c05ce25e3d0459
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2023-01-25 23:07:19 +0100

    refactor: ensure EmailValidatorAdapter call validator with correct email

--- api/src/utils/email-validator-adapter.spec.ts
+++ api/src/utils/email-validator-adapter.spec.ts
@@ -8,6 +8,7 @@ jest.mock('validator', () => ({
   }
 }))
 
+
 describe('EmailValidatorAdapter', () => {
 
   test('Should return false if validator returns false', () => {
@@ -21,6 +22,13 @@ describe('EmailValidatorAdapter', () => {
     const sut = new EmailValidatorAdapter()
     const isValid = sut.isValid('invalid_email')
     expect(isValid).toBe(true)
+  })
+
+  test('Should call validator with correct email', () => {
+    const sut = new EmailValidatorAdapter()
+    const isEmailSpy = jest.spyOn(validator, 'isEmail')
+    sut.isValid('any_email@mail.com')
+    expect(isEmailSpy).toHaveBeenCalledWith('any_email@mail.com')
   })
 
 
